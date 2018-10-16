c97a99ae1c116b70c4d6a5af02a1a3e8eef976ef
Jakub Drapala <jakubdrapala@gmail.com>
2018-10-16 11:27:27 +0200

    Now data.sql file is shorter and cleaner due to a bit another sql's commands

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,88 +1,52 @@
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(101, 'Wisła', 'Odra', 'Dunajec', 'San');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(102, 'Rysy', 'Giewont', 'Śnieżka', 'Tarnica');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(103, 'Andy', 'Pireneje', 'Himalaje', 'Alpy');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(104, 'Pakistan', 'Mongolia', 'Kazachstan', 'Rosja');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(105, 'Mont Blanc', 'Mount Everest', 'K2', 'Rysy');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(106, 'Rosji', 'Poski', 'Ukrainy', 'Stanów Zjednoczonych');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(107, 'Azja', 'Ameryka Południowa', 'Europa', 'Ameryka Północna');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(108, 'Afryki', 'Azji', 'Europy', 'Ameryki Północnej');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(109, 'Missisipi', 'Amazonka', 'Missouri', 'Jangcy');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(110, 'Tanzania', 'Kongo', 'Kolumbia', 'Indonezja');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(111, 'Dunaj', 'Ren', 'Łaba', 'Odra');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(112, 'Egipt', 'Etiopia', 'Wybrzeże Kości Słoniowej', 'Nigeria');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(113, 'Górne', 'Michigan', 'Wielkie Niewolnicze', 'Wiktorii');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(114, 'Kaukazu', 'Karpat', 'Gór Skandynawskich', 'Gór Smoczych');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(115, 'Gerlach', 'Łomnica', 'Rysy', 'Korab');
+insert into answers(id, correct, incorrect1, incorrect2, incorrect3) values
+(101, 'Wisła', 'Odra', 'Dunajec', 'San'),
+(102, 'Rysy', 'Giewont', 'Śnieżka', 'Tarnica'),
+(103, 'Andy', 'Pireneje', 'Himalaje', 'Alpy'),
+(104, 'Pakistan', 'Mongolia', 'Kazachstan', 'Rosja'),
+(105, 'Mont Blanc', 'Mount Everest', 'K2', 'Rysy'),
+(106, 'Rosji', 'Poski', 'Ukrainy', 'Stanów Zjednoczonych'),
+(107, 'Azja', 'Ameryka Południowa', 'Europa', 'Ameryka Północna'),
+(108, 'Afryki', 'Azji', 'Europy', 'Ameryki Północnej'),
+(109, 'Missisipi', 'Amazonka', 'Missouri', 'Jangcy'),
+(110, 'Tanzania', 'Kongo', 'Kolumbia', 'Indonezja'),
+(111, 'Dunaj', 'Ren', 'Łaba', 'Odra'),
+(112, 'Egipt', 'Etiopia', 'Wybrzeże Kości Słoniowej', 'Nigeria'),
+(113, 'Górne', 'Michigan', 'Wielkie Niewolnicze', 'Wiktorii'),
+(114, 'Kaukazu', 'Karpat', 'Gór Skandynawskich', 'Gór Smoczych'),
+(115, 'Gerlach', 'Łomnica', 'Rysy', 'Korab');
 
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(201, 'Bolesław Chrobry', 'Mieszko I', 'Bolesław Śmiały', 'Władysław Jagiełło');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(202, '966 r.', '965  r.', '996 r.', '995 r.');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(203, 'Ulrich von Jungingen', 'Konrad von Wallenrode', 'Heinrich V von Plauen', 'Karl von Habsburg-Lothringen');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(204, 'Kurskiem', 'Moskwą', 'Stalingradem', 'Wołgogradem');
-insert into answers(id, correct, incorrect1, incorrect2, incorrect3)
-values(205, '1919 r.', '1917 r.', '1916 r.', '1920 r.');
+insert into answers(id, correct, incorrect1, incorrect2, incorrect3) values
+(201, 'Bolesław Chrobry', 'Mieszko I', 'Bolesław Śmiały', 'Władysław Jagiełło'),
+(202, '966 r.', '965  r.', '996 r.', '995 r.'),
+(203, 'Ulrich von Jungingen', 'Konrad von Wallenrode', 'Heinrich V von Plauen', 'Karl von Habsburg-Lothringen'),
+(204, 'Kurskiem', 'Moskwą', 'Stalingradem', 'Wołgogradem'),
+(205, '1919 r.', '1917 r.', '1916 r.', '1920 r.');
 
 
 
-insert into question(id, content, category, answers_id)
-values(1, 'Jaka jest najdłuższa rzeka w Polsce?', 'geography', 101);
-insert into question(id, content, category, answers_id)
-values(2, 'Jaki jest najwyższy szczyt górski w Polsce?', 'geography', 102);
-insert into question(id, content, category, answers_id)
-values(3, 'Które z wymienionych pasm górskim leży w Ameryce Południowej?', 'geography', 103);
-insert into question(id, content, category, answers_id)
-values(4, 'Które z wymienionych państw ma w swych granicach Himalaje?', 'geography', 104);
-insert into question(id, content, category, answers_id)
-values(5, 'Jaki jest najwyższy szczyt Europy?', 'geography', 105);
-insert into question(id, content, category, answers_id)
-values(6, 'Jezioro Bajkał znajduje się na terytorium?', 'geography', 106);
-insert into question(id, content, category, answers_id)
-values(7, 'Kontynent na terytorium, którego znajduje się cieśnina Malakka to:', 'geography', 107);
-insert into question(id, content, category, answers_id)
-values(8, 'Kilimandżaro to najwyższy szczyt:', 'geography', 108);
-insert into question(id, content, category, answers_id)
-values(9, 'Najdłuższa Rzeka Ameryki Północnej to:', 'geography', 109);
-insert into question(id, content, category, answers_id)
-values(10, 'Przez, które z wymienionych państw nie przechodzi równik:', 'geography', 110);
-insert into question(id, content, category, answers_id)
-values(11, 'Najdłuższa rzeka Niemiec to:', 'geography', 111);
-insert into question(id, content, category, answers_id)
-values(12, 'Którego z wymienionych Państw nie zaliczamy do Afryki Subsaharyjskiej?', 'geography', 112);
-insert into question(id, content, category, answers_id)
-values(13, 'Jak nazywa się największe jezioro Ameryki Północnej?', 'geography', 113);
-insert into question(id, content, category, answers_id)
-values(14, 'Elbrus to najwyższy szczyt?', 'geography', 114);
-insert into question(id, content, category, answers_id)
-values(15, 'Najwyższy szczyt Karpat to:', 'geography', 115);
+insert into question(id, content, category, answers_id) values
+(1, 'Jaka jest najdłuższa rzeka w Polsce?', 'geography', 101),
+(2, 'Jaki jest najwyższy szczyt górski w Polsce?', 'geography', 102),
+(3, 'Które z wymienionych pasm górskim leży w Ameryce Południowej?', 'geography', 103),
+(4, 'Które z wymienionych państw ma w swych granicach Himalaje?', 'geography', 104),
+(5, 'Jaki jest najwyższy szczyt Europy?', 'geography', 105),
+(6, 'Jezioro Bajkał znajduje się na terytorium?', 'geography', 106),
+(7, 'Kontynent na terytorium, którego znajduje się cieśnina Malakka to:', 'geography', 107),
+(8, 'Kilimandżaro to najwyższy szczyt:', 'geography', 108),
+(9, 'Najdłuższa Rzeka Ameryki Północnej to:', 'geography', 109),
+(10, 'Przez, które z wymienionych państw nie przechodzi równik:', 'geography', 110),
+(11, 'Najdłuższa rzeka Niemiec to:', 'geography', 111),
+(12, 'Którego z wymienionych Państw nie zaliczamy do Afryki Subsaharyjskiej?', 'geography', 112),
+(13, 'Jak nazywa się największe jezioro Ameryki Północnej?', 'geography', 113),
+(14, 'Elbrus to najwyższy szczyt?', 'geography', 114),
+(15, 'Najwyższy szczyt Karpat to:', 'geography', 115);
 
-insert into question(id, content, category, answers_id)
-values(21, 'Kto był pierwszym królem Polski?', 'history', 201);
-insert into question(id, content, category, answers_id)
-values(22, 'Datę chrztu Polski określa się na:', 'history', 202);
-insert into question(id, content, category, answers_id)
-values(23, 'Wielkim Mistrzem Zakonu Krzyżackiego, pokonanym pod Grunwaldem był:', 'history', 203);
-insert into question(id, content, category, answers_id)
-values(24, 'Największa bitwa pancera II Wojny Światowej odbyła się pod:', 'history', 204);
-insert into question(id, content, category, answers_id)
-values(25, 'Traktat Wersalski na mocy którego Polska odzyskała niepodległość podpisano w: ', 'history', 205);
+insert into question(id, content, category, answers_id) values
+(21, 'Kto był pierwszym królem Polski?', 'history', 201),
+(22, 'Datę chrztu Polski określa się na:', 'history', 202),
+(23, 'Wielkim Mistrzem Zakonu Krzyżackiego, pokonanym pod Grunwaldem był:', 'history', 203),
+(24, 'Największa bitwa pancera II Wojny Światowej odbyła się pod:', 'history', 204),
+(25, 'Traktat Wersalski na mocy którego Polska odzyskała niepodległość podpisano w: ', 'history', 205);
 
 
 
