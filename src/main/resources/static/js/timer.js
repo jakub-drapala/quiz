27431652303787e063b300386273681d1f97a6a6
Jakub Drapala <jakubdrapala@gmail.com>
2019-07-08 17:39:26 +0200

    Deploying timer is successful

--- src/main/resources/static/js/timer.js
+++ src/main/resources/static/js/timer.js
@@ -1,16 +1,12 @@
-if (document.cookie.length == 0) {
-    Cookies.set('val', '10');
-}
-
-
-var time = Cookies.get('val');
-
+let time = localStorage.getItem('time');
 function odliczanie()
 	{
-		document.getElementById("zegar").innerHTML =
-		 time;
+	console.log(localStorage.getItem('time'));
+
+		document.getElementById("zegar").innerHTML = time;
 
 		 setTimeout("odliczanie()",1000);
 		 time++;
 		 Cookies.set('val', time);
+		 localStorage.setItem('time', time);
 	}
